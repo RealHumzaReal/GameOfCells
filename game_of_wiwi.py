3b8925d89f9c644789f812d57c4cf339d5a70fb6
RealHumzaReal <huali@smail.pembinatrails.ca>
2026-03-27 09:12:38 -0500

    Created the main logic of the game
    The main logic, where if the cell is surrounded by less then 3 cells, it dies and when it isn't, it lives. Has been created. A bit of the visual aspect has also been made.

--- game_of_wiwi.py
+++ game_of_wiwi.py
@@ -10,5 +10,25 @@ COLOR_ALIVE_NEXT = (10, 255, 75)
 
 #Function that updates cells. (Main function/Logic of the game)
 def update(screen, cells, size, with_progress=False):
-	updated_cells = np.empty((cells.shape[0], cells.shape[1]))
+	updated_cells = np.empty((cells.shape[0], cells.shape[1])) #Creates shape of the alreaddy existed cells.
 
+	for row, col in np.ndindex(cells.shape): #Takes each individual cell that alreaddy exists and applies rules of the game to it.
+		alive = np.sum(cells[row-1:row+2, col-1:col+2]) - cells[row, col]
+		color = COLOR_BG if cells[row, col] == 0 else COLOR_ALIVE_NEXT
+
+		if cells[row, col] == 1:
+			if alive < 2 or alive > 3
+				if with_progress:
+					color = COLOR_DIE_NEXT
+			elif 2 <= alive <= 3:
+				updated_cells[row, col] = 1
+				if with_progress:
+					color = COLOR_ALIVE_NEXT
+		else:
+			if alive == 3:
+				updated_cells[row, col] = 1
+				if with_progress:
+					color = COLOR_ALIVE_NEXT
+
+		pygame.draw.rect(screen, color, (col * size, row * size, size - 1, size - 1)) #Draws cells onto the chart (Visual aspect)
+	return_updated_cells
